a54f87f88cfd3878ad1830b7b87fcc94328f43fb
khanhhung59 <vokhanhhung299@gmail.com>
2024-09-05 16:43:59 +0700

    XCode update

--- luyen/main.cpp
+++ luyen/main.cpp
@@ -1,47 +1,29 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 int main()
 {
-    long n;
-    cin>>n;
-    long left = 1;
-    long right = n;
-    long Min = n;
-    while(left <= right)
+    long long N , Q;
+    cin>>N>>Q;
+    long long K[N];
+    for(long long i = 1; i <= N ; i++)
     {
-        long mid =(left + right) / 2;
-        long sole,sochang,tongchang;
-        if(mid % 2 == 0)
-        {
-            sole = ((mid - 1) - 1) / 2  + 1;
-            sochang = (n - (mid + 2)) / 2 + 1;
-            tongchang = (sochang * ((mid + 2) * 2 +(sochang - 1) * 2))/2;
-        }
-        else
-        {
-            sole = (mid - 1) / 2  + 1;
-            sochang = ((n - (mid + 1)) ) / 2 + 1;
-            tongchang = (sochang * ((mid + 1) * 2 +(sochang - 1) * 2))/2;
-        }
-        long tongle = (sole * (2 + (sole -1) * 2))/2;
-        if(tongle < tongchang)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-            if(Min > mid)
-            {
-                Min = mid;
-            }
-        }
-        //cout<<mid<<"\n";
-        //cout<<mid<<" "<<sole<<" "<<sochang;
-        //cout<<tongle<<" "<<tongchang<<"\n\n";
+        cin>>K[i];
     }
-    cout<< Min;
-    system("pause");
+    for(long long i = 1; i <= Q; i++)
+    {
+        long long L, R;
+        cin>>L >> R;
+        long long DoDai = R - L + 1;
+        long long j = 1;
+        long long Tong = 0;
+        long long Dem = L;
+        while(j <= DoDai)
+        {
+            Tong += K[Dem] * j;
+            Dem++;
+            j++;
+        }
+        cout<<Tong<<endl;
+    }
     return 0;
 }
